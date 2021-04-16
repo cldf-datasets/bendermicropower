d1ee16df553a75eec7ad87268db01e4a17c85460
Christoph Rzymski <christoph@functor-argument.net>
2021-04-16 13:13:23 +0200

    Add tests, release prep

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-
 def test_valid(cldf_dataset, cldf_logger):
     assert cldf_dataset.validate(log=cldf_logger)
+
+
+def test_forms(cldf_dataset):
+    assert len(list(cldf_dataset["FormTable"])) == 229
+    assert any(f["Form"] == "eṅoal" for f in cldf_dataset["FormTable"])
+
+
+def test_parameters(cldf_dataset):
+    assert len(list(cldf_dataset["ParameterTable"])) == 10
+
+
+def test_languages(cldf_dataset):
+    assert len(list(cldf_dataset["LanguageTable"])) == 38
